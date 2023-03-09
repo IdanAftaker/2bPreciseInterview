b2893048b76d6eead34f30480e1852e9f1650326
Idan Aftaker <idana@earnix.com>
2023-03-09 08:24:10 +0200

    Initial commit

---
+++ src/test/java/com/example/interview2bprecise/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.interview2bprecise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
